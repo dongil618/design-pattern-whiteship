80fb365ad915d084da27f4b4290be766618c3889
dongil618 <rlaehddlf618@gmail.com>
2023-07-20 20:59:08 +0900

    chore : Initial Commit

---
+++ src/main/java/com/example/designpatternwhiteship/DesignPatternWhiteshipApplication.java
@@ -0,0 +1,13 @@
+package com.example.designpatternwhiteship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternWhiteshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternWhiteshipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/designpatternwhiteship/DesignPatternWhiteshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.designpatternwhiteship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternWhiteshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
